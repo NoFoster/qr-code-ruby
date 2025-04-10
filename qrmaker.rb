3c37ca0feefc823edc21b059ef0ccd7273040138
NoFoster <kfoster0@chicagobooth.edu>
2025-04-10 21:45:09 +0000

    rprcode addition

--- qrmaker.rb
+++ qrmaker.rb
@@ -1,1 +1,7 @@
-pp "hello world"
+##gem install rqrcode
+
+
+require "rqrcode"
+qrcode = RQRCode::QRCode.new("wikipedia qr code")
+png = qrcode.as_png({ :size => 500})
+IO.binwrite("sometext.png", png.to_s)
